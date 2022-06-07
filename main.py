ab904dbdbc1bae06349b359f9cb4403cf0484373
karan684 <51419129+karan684@users.noreply.github.com>
2022-06-07 09:58:00 +0530

    Add files via upload
    A race between 6 turtles

---
+++ main.py
@@ -0,0 +1,36 @@
+from turtle import Turtle, Screen
+import  random
+
+screen = Screen()
+screen.setup(width = 500, height = 400)
+is_race_on = False
+user_bet = screen.textinput("Make a bet", "Which turtle will win? Choose your color")
+color = ["red", "orange", "yellow", "blue", "purple", "green"]
+y_pos = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
+
+for i in range (0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(color[i])
+    new_turtle.goto(x = -230, y = y_pos[i])
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've Won! The {winning_color} turtle is the winner")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner")
+        distance = random.randint(0, 10)
+        turtle.forward(distance)
+
+
+screen.exitonclick()
